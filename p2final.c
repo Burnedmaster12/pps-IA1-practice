0dd58c332b0fc0306dee57af90d29ea6f70b0929
Faizal Hussain <96277916+Burnedmaster12@users.noreply.github.com>
2022-02-04 04:47:09 +0000

    p2 final done

---
+++ p2final.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int input()
+{
+  int a;
+  printf("enter the number\n");
+  scanf("%d",&a);
+  return a;
+}
+int compare(int a, int b, int c)
+{
+  if(a>b && a>c)
+  return a;
+  else if (b>a && b>c)
+  return b;
+  else 
+  return c;
+}
+int print(int a, int b, int c, int l)
+{
+  printf("the greates of %d %d %d is %d",a,b,c,l);
+}
+int main()
+{
+  int x,y,z,lar;
+  x=input();
+  y=input();
+  z=input();
+  lar=compare(x,y,z);
+  print(x,y,z,lar);
+
+}
